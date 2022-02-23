cac9f3c917d97b0972fc52aa5ea72d80de4e5adc
Derek Hsieh <devderekhsieh@gmail.com>
2022-02-23 15:27:42 -0800

    Initial Commit

---
+++ ArroSocial/ArroSocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArroSocialApp.swift
+//  ArroSocial
+//
+//  Created by Derek Hsieh on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ArroSocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
