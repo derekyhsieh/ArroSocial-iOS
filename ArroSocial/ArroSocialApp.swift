8542acbc3b9778daa6691083de5330f410ac0b5d
Derek Hsieh <devderekhsieh@gmail.com>
2022-03-02 23:07:30 -0800

    connected to firebase with swift package manager

--- ArroSocial/ArroSocialApp.swift
+++ ArroSocial/ArroSocialApp.swift
@@ -6,9 +6,30 @@
 //
 
 import SwiftUI
+import Firebase
+
+// add this if we need firebase cloud messsaging and other firebase services later
+
+    /*
+     class AppDelegate: NSObject, UIApplicationDelegate {
+       func application(_ application: UIApplication,
+                        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+         print("Colors application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+         FirebaseApp.configure()
+         return true
+       }
+     }
+    */
 
 @main
 struct ArroSocialApp: App {
+    
+    init() {
+        // connect app to firebase
+        FirebaseApp.configure()
+    }
+    
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
