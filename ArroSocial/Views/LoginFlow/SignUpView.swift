4b254de22c29add412e5e1e54a5f2319e84b6e08
Derek Hsieh <devderekhsieh@gmail.com>
2022-03-04 09:35:04 -0800

    add email sanitization in sign up view

--- ArroSocial/Views/LoginFlow/SignUpView.swift
+++ ArroSocial/Views/LoginFlow/SignUpView.swift
@@ -166,10 +166,12 @@ struct SignUpView: View {
     }
     
     func isValidEmail(_ email: String) -> Bool {
+        // email sanitization
+        let cleanedEmail = email.trimmingCharacters(in: .whitespacesAndNewlines)
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         
         let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: email)
+        return emailPred.evaluate(with: cleanedEmail)
     }
     
     func isValidPassword(_ password: String) -> Bool {
